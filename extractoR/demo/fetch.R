1806b26ef5e179189267053f56ade09a8209e3a3
Maëlick Claes <himself@maelick.net>
2013-09-13 12:01:16 -0400

    Mirror added in demo

--- extractoR/demo/fetch.R
+++ extractoR/demo/fetch.R
@@ -1,12 +1,14 @@
 library(extractoR.fetch)
 
+mirror <- "http://cran.parentingamerica.com"
+
 print("Fetching package list from CRAN")
-t <- system.time(packages <- FetchCRANList())
+t <- system.time(packages <- FetchCRANList(mirror))
 print(sprintf("Package list fetched from CRAN in %.3fs", t[3]))
 
 SavePackagesList(packages, "packages/packages.yml")
 printf("Package list saved to packages/packages.yml")
 
 printf("Downloading missing packages")
-t <- system.time(res <- FetchPackages(packages, "packages"))[3]
+t <- system.time(res <- FetchPackages(packages, "packages", mirror))[3]
 print("%n packages downloaded in %.3fs", length(res[res]), t[3])
