2b3dec262cf1d275c42b0ffc5ffebf4f0e0208e2
Maëlick Claes <himself@maelick.net>
2013-09-13 10:00:12 -0400

    Descfiles: GuessEncoding

--- extractoR.extract/R/descfiles.R
+++ extractoR.extract/R/descfiles.R
@@ -26,10 +26,11 @@ ReadDescfile <- function(package, version, datadir) {
   if (file.exists(name)) {
     descfile <- read.dcf(name)
     values <- as.vector(descfile[1, ])
+    encoding <- GuessEncoding(name)
     if ("Encoding" %in% colnames(descfile)) {
       encoding <- descfile[colnames(descfile) == "Encoding"]
-      values <- iconv(as.vector(descfile[1, ]), encoding, "utf8")
     }
+    values <- iconv(as.vector(descfile[1, ]), encoding, "utf8")
     n <- ncol(descfile)
     data.frame(package=rep(package, n), version=rep(version, n),
                key=colnames(descfile), value=values, stringsAsFactors=FALSE)
