bf29d623ba06961b7f10e46cf6d75ef92d18b0a9
Maëlick Claes <himself@maelick.net>
2013-09-30 11:03:39 -0400

    InsertCRANCheckings warning when unfound status

--- DepsCheckeR/R/sql.R
+++ DepsCheckeR/R/sql.R
@@ -57,8 +57,11 @@ GetHashCRANStatus <- function(con, date) {
 
 InsertCRANChecking <- function(con, checking) {
   status <- GetHashCRANStatus(con, unique(checking$date))
-  checking <- checking[sapply(apply(checking, 1, GetCRANStatusKey),
-                              has.key, status), ]
+  has.status <- sapply(apply(checking, 1, GetCRANStatusKey), has.key, status)
+  if (any(!has.status)) {
+    warning(sprintf("%d unfound status", length(has.status[!has.status])))
+    checking <- checking[has.status, ]
+  }
   status <- apply(checking, 1, function(s) status[[GetCRANStatusKey(s)]])
   types <- FormatString(con, as.vector(checking$check))
   status.types <- FormatString(con, as.character(checking$status))
