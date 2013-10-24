eee399ee7ce739df2af1fb900c6029b15f8ec307
Maëlick Claes <himself@maelick.net>
2013-10-24 23:05:37 +0200

    Installation script added

---
+++ install.R
@@ -0,0 +1,8 @@
+library(devtools)
+
+install("extractoR.utils")
+install("extractoR.fetch")
+install("extractoR.sql")
+install("extractoR.extract")
+install("extractoR.checkings")
+install("extractoR")
