4558cbe98334deb97c419d99995c7a801eec670a
Maëlick Claes <himself@maelick.net>
2013-11-18 16:04:51 +0100

    Extract http log: dataframe stringsAsFactors set to false

--- extractoR.extract/R/httplogs.R
+++ extractoR.extract/R/httplogs.R
@@ -1,5 +1,5 @@
 ExtractHttpLog <- function(logfile) {
-  log <- read.csv2(logfile)
+  log <- read.csv2(logfile, stringsAsFactors=FALSE)
   log$date <- paste(log$date, log$time)
   log$time <- NULL
   log
