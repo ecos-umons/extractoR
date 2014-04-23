9d29642681396ef541ebe4e43cd0a3f76f42f22d
Maëlick Claes <himself@maelick.net>
2014-04-23 16:54:58 +0200

    extractoR.content refactoring using CloneR

--- extractoR.content/R/parse.R
+++ extractoR.content/R/parse.R
@@ -1,18 +1,6 @@
 ParsePackage <- function(package, version, path, fcode, fnull, ferr,
                          guess.encoding=FALSE) {
-  ParseFile <- function(filename) {
-    encoding <- "unknown"
-    if (guess.encoding) encoding <- GuessEncoding(filename)
-    parse(filename, keep.source=TRUE, encoding=encoding)
-  }
-  Parse <- function(path) {
-    src <- grep("\\.R$", dir(file.path(path, "R"), full.names=TRUE),
-                ignore.case=TRUE, value=TRUE)
-    if (length(src)) {
-      do.call(c, lapply(src, ParseFile))
-    }
-  }
-  res <- tryCatch(Parse(path), error=function(e) e[[1]])
+  res <- tryCatch(CloneR::ParsePackage(path), error=function(e) e[[1]])
   data <- list(package=package, version=version, path=path)
   if (is.null(res)) {
     fnull(data)
