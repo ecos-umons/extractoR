8f038a8fd709b4cdedfc889719ef200955a65974
Maëlick Claes <himself@maelick.net>
2014-06-13 16:17:11 +0200

    content.R script added

---
+++ scripts/content.R
@@ -0,0 +1,17 @@
+source("scripts/main.R")
+
+options(expressions = 100000)
+
+library(CloneR)
+library(extractoR.content)
+
+rdata <- LoadRData("/data/cran/rds")
+packages <- rdata$packages[c("package", "version")]
+
+datadir <- "/data/cran"
+pkg.dir <- file.path(datadir, "packages")
+func.dir <- file.path(datadir, "functions")
+log.dir <- file.path(datadir, "log/functions")
+
+system.time(res <- BrowseFunctions(packages, pkg.dir, func.dir, log.dir))
+print(table(sapply(res, function(x) x$res)))
