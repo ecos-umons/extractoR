128a46d8e576d63fe0bc1164436f96306baf98b2
Maëlick Claes <himself@maelick.net>
2015-04-07 16:38:09 +0200

    extractoR.data updated

--- extractoR.data/R/rdata.R
+++ extractoR.data/R/rdata.R
@@ -1,14 +1,26 @@
-SaveRData <- function(rdata, datadir) {
+SaveRData <- function(rdata, datadir, format="rds", FUNC=saveRDS, ...) {
+  datadir <- file.path(datadir, format)
   dir.create(datadir, recursive=TRUE, showWarnings=FALSE)
   for (name in names(rdata)) {
-    filename <- file.path(datadir, sprintf("%s.rds", name))
-    saveRDS(rdata[[name]], file=filename)
+    filename <- file.path(datadir, sprintf("%s.%s", name, format))
+    FUNC(rdata[[name]], file=filename, ...)
   }
 }
 
-LoadRData <- function(datadir) {
-  files <- grep("\\.rds$", dir(datadir), value=TRUE)
-  rdata <- lapply(files, function(f) readRDS(file.path(datadir, f)))
-  names(rdata) <- sub("\\.rds$", "", files)
+LoadRData <- function(datadir, format="rds", FUNC=readRDS, ...) {
+  datadir <- file.path(datadir, format)
+  extension <- sprintf("\\.%s$", format)
+  files <- grep(extension, dir(datadir), value=TRUE)
+  rdata <- lapply(files, function(f) FUNC(file.path(datadir, f), ...))
+  names(rdata) <- sub(extension, "", files)
   rdata
+}
+
+SaveCSV <- function(rdata, datadir) {
+  SaveRData(rdata, datadir, format="csv", FUNC=write.csv, row.names=FALSE)
+}
+
+LoadCSV <- function(datadir) {
+  cran <- LoadRData(datadir, format="csv", FUNC=read.csv, stringsAsFactor=FALSE)
+  lapply(cran, as.data.table)
 }
