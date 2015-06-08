1ee62a939361bfda1c4f500903f1339b64ecede7
Maëlick Claes <himself@maelick.net>
2015-06-08 12:58:18 +0200

    DESCRIPTION files extraction updated

--- extractoR.extract/R/descfiles.R
+++ extractoR.extract/R/descfiles.R
@@ -1,10 +1,10 @@
-DescfileName <- function(package, version, datadir) {
-  # Returns the file name of a DESCRIPTION file.
-  file.path(datadir, "packages", package, version, package, "DESCRIPTION")
+GuessEncoding <- function(filename) {
+  # Guesses the encoding of a file.
+  cmd <- sprintf("file --mime-encoding %s", filename)
+  strsplit(system(cmd, intern=TRUE), " ")[[1]][2]
 }
 
 ReadDescfile <- function(filename, package=NA, version=NA) {
-  ## message(sprintf("Reading DESCRIPTION file %s", filename))
   if (file.exists(filename)) {
     descfile <- read.dcf(filename)
     values <- as.vector(descfile[1, ])
@@ -17,16 +17,45 @@ ReadDescfile <- function(filename, package=NA, version=NA) {
     res <- as.data.table(list(key=colnames(descfile), value=values))
     if (is.na(package) | is.na(version)) res
     else cbind(data.table(package=package, version=version), res)
-  } else NULL
+  }
 }
 
-CRANDescfiles <- function(cran, datadir) {
-  cran <- unique(cran[, list(package, version)])
-  packages <- cran$package
-  versions <- cran$version
-  filenames <- mapply(DescfileName, packages, versions,
-                      MoreArgs=list(datadir), SIMPLIFY=FALSE)
-  rbindlist(mapply(ReadDescfile, filenames, packages, versions, SIMPLIFY=FALSE))
+ReadCRANDescfile <- function(package, version, datadir) {
+  loginfo("Parsing CRAN DESCRIPTION file from %s %s",
+          package, version, logger="extract.descfile.cran")
+  filename <- file.path(datadir, package, version, package, "DESCRIPTION")
+  descfile <- ReadDescfile(filename, package, version)
+  if (!is.null(descfile)) cbind(data.table(source="cran"), descfile)
+}
+
+ReadGithubDescfile <- function(package, ref, datadir) {
+  loginfo("Parsing Github DESCRIPTION file from package %s %s",
+          package, ref, logger="extract.descfile.github")
+  repo.name <- ParseGithubRepositoryName(package)
+  RunGit(function() {
+    filename <- file.path(repo.name$subdir, "DESCRIPTION")
+    status <- system2("git", c("checkout", ref, filename))
+    if (!status) {
+      res <- tryCatch(ReadDescfile(filename), error=function(e) NULL)
+      system2("git", c("checkout", "HEAD", filename))
+      if (!is.null(res)) {
+        cbind(data.table(source="cran", package, version=ref), res)
+      }
+    }
+  }, file.path(datadir, repo.name$owner, repo.name$repository))
+}
+
+Descfiles <- function(index, datadir) {
+  rbindlist(mapply(function(src, repository, version) {
+    dir <- file.path(datadir, src)
+    if (src == "cran") {
+      ReadCRANDescfile(repository, version, file.path(dir, "packages"))
+    } else if (src == "github") {
+      ReadGithubDescfile(repository, version, file.path(dir, "repos"))
+    } else {
+      stop(sprintf("Unknown source: %s", src))
+    }
+  }, index$source, index$repository, index$version, SIMPLIFY=FALSE))
 }
 
 IsPackage <- function(descfile) {
--- extractoR.extract/R/utils.R
+++
@@ -1,5 +0,0 @@
-GuessEncoding <- function(filename) {
-  # Guesses the encoding of a file.
-  cmd <- sprintf("file --mime-encoding %s", filename)
-  strsplit(system(cmd, intern=TRUE), " ")[[1]][2]
-}
