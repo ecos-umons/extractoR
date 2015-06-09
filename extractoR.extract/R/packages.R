dbcc5f64335c5ea49e7ccf04c7c4396e2e517b3f
Maëlick Claes <himself@maelick.net>
2015-06-09 11:43:24 +0200

    Bugfix

--- extractoR.extract/R/packages.R
+++ extractoR.extract/R/packages.R
@@ -1,15 +1,15 @@
 Duplicates <- function(packages) {
-  packages[, if (.N > 1) list(package=unique(package)),
+  packages[, if (.N > 1) list(repository=unique(repository)),
            by=c("source", "Package")]
 }
 
 Uniques <- function(packages) {
-  packages[, if (.N == 1) list(package=unique(package)),
+  packages[, if (.N == 1) list(repository=unique(repository)),
            by=c("source", "Package")]
 }
 
 PackagesMatchingRepository <- function(packages, tolower=FALSE) {
-  repos <- ParseGithubRepositoryName(packages$package)$repository
+  repos <- ParseGithubRepositoryName(packages$repository)$repository
   if (tolower) packages[tolower(Package) == tolower(repos)]
   else packages[Package == repos]
 }
@@ -27,6 +27,6 @@ Packages <- function(descfiles, broken) {
   res <- Uniques(PackagesMatchingRepository(Duplicates(packages)))
   res <- rbind(Uniques(packages), res)
   res <- merge(res, metadatas, by=c("source", "repository", "Package"))
-  res[, c(list(source=source, repository, ref),
+  res[, c(list(source=source, repository=repository, ref=ref),
           lapply(keys, get, envir=environment()))]
 }
