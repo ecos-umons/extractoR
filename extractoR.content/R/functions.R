2f515635144f912fcdb0516102f6566415362fdd
Maëlick Claes <himself@maelick.net>
2015-07-17 09:58:03 +0200

    extractoR.content update to work with sourceR

---
+++ extractoR.content/R/functions.R
@@ -0,0 +1,29 @@
+FunctionDefinitions <- function(package) {
+  loginfo("Parsing R functions from package %s %s %s",
+          package$source, package$repository, package$ref, logger="functions")
+  if (!is.null(package$code)) {
+    package <- c(package, sourceR::FunctionDefinitions(package$code))
+  }
+  package$code <- NULL
+  package
+}
+
+FunctionCalls <- function(package) {
+  loginfo("Parsing R function calls from package %s %s %s",
+          package$source, package$repository, package$ref, logger="functions")
+  if (!is.null(package$code)) {
+    package <- c(package, sourceR::FunctionCalls(package$code))
+  }
+  package$code <- NULL
+  package
+}
+
+CodingStyle <- function(package) {
+  loginfo("Parsing R functions from package %s %s %s",
+          package$source, package$repository, package$ref, logger="functions")
+  if (!is.null(package$code)) {
+    package <- c(package, sourceR::CodingStyle(package$code))
+  }
+  package$code <- NULL
+  package
+}
--- extractoR.content/R/parse.R
+++ extractoR.content/R/parse.R
@@ -1,14 +1,41 @@
-ParsePackage <- function(package, version, path, fcode, fnull, ferr,
-                         guess.encoding=FALSE) {
-  res <- tryCatch(cloneR::ParsePackage(path), error=function(e) e[[1]])
-  data <- list(package=package, version=version, path=path)
-  if (is.null(res)) {
-    fnull(data)
-  } else if (is.expression(res)) {
-    data$code <- res
-    fcode(data)
-  } else if (!is.null(res)) {
-    data$err <- res
-    ferr(data)
+ParseCRANPackage <- function(package, version, datadir) {
+  loginfo("Parsing R code from CRAN package %s %s",
+          package, version, logger="code.cran")
+  filename <- file.path(datadir, package, version, package)
+  tryCatch(sourceR::ParsePackage(filename), error=identity)
+}
+
+ParseGithubPackage <- function(package, ref, datadir) {
+  loginfo("Parsing R code from Github package %s %s",
+          package, ref, logger="code.github")
+  repo.name <- ParseGithubRepositoryName(package)
+  RunGit(function() {
+    filename <- file.path(repo.name$subdir, "R")
+    status <- system2("git", c("checkout", ref, filename))
+    if (!status) {
+      res <- tryCatch(sourceR::ParsePackage(dirname(filename)), error=identity)
+      system2("git", c("checkout", "HEAD", filename))
+      res
+    }
+  }, file.path(datadir, repo.name$owner, repo.name$repository))
+}
+
+ParsePackage <- function(source, repository, ref, datadir) {
+  dir <- file.path(datadir, source)
+  if (source == "cran") {
+    res <- ParseCRANPackage(repository, ref, file.path(dir, "packages"))
+  } else if (source == "github") {
+    res <- ParseGithubPackage(repository, ref, file.path(dir, "repos"))
+  } else {
+    stop(sprintf("Unknown source: %s", src))
   }
+  data <- list(source=source, repository=repository, ref=ref)
+  if (is.list(res)) {
+    data$code <- res
+  } else if (inherits(res, "error")) {
+    data$err <- res
+  } else if (!is.null(res)) {
+    stop("Unknown result")
+  }
+  data
 }
