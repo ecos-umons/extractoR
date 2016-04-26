91b41a486038bd89a9dbb14e01c139e4a8a5ba36
Maëlick Claes <himself@maelick.net>
2016-04-26 11:19:46 +0200

    TestGithubRepository added

--- extractoR.github/R/repos.R
+++ extractoR.github/R/repos.R
@@ -1,3 +1,10 @@
+TestGithubRepository <- function(owner, name, subdir=".") {
+  url <- sprintf("https://github.com/%s/%s/blob/master/%s/DESCRIPTION",
+                 owner, name, subdir)
+  loginfo("Checking %s/%s/%s", owner, name, subdir, logger="github.test")
+  HTTPCheck(url)
+}
+
 FetchGithubRepository <- function(owner, name, datadir, update=TRUE) {
   url <- sprintf("https://github.com/%s/%s.git", owner, name)
   dest <- file.path(datadir, owner, name)
