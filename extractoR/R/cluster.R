1a7e1cf23620ed517d3dc0013be76a030f63c25d
Maëlick Claes <himself@maelick.net>
2016-04-26 12:06:15 +0200

    Calls to library removed in InitCluster

--- extractoR/R/cluster.R
+++ extractoR/R/cluster.R
@@ -2,11 +2,8 @@ InitCluster <- function(logger, logfile, n=4) {
   cl <- makeCluster(n, type="PSOCK", outfile="")
   clusterExport(cl, list(logfile="logfile"), envir=environment())
   clusterCall(cl, function() {
-    library(logging)
-    library(RCurl)
-
-    basicConfig()
-    addHandler(writeToFile, logger=logger, file=logfile)
+    logging::basicConfig()
+    logging::addHandler(writeToFile, logger=logger, file=logfile)
   })
   cl
 }
