eaeaf2c219696d39fee4b91326eac99fed0ead3e
Maëlick Claes <himself@maelick.net>
2016-04-28 10:07:55 +0200

    LoadRData and SaveRData: subdir option added

--- extractoR.data/R/rdata.R
+++ extractoR.data/R/rdata.R
@@ -1,47 +1,54 @@
-SaveRData <- function(rdata, datadir, format="rds", FUNC=saveRDS, ...) {
-  datadir <- file.path(datadir, format)
+SaveRData <- function(rdata, datadir, subdir=".", format="rds",
+                      FUNC=saveRDS, ...) {
+  datadir <- file.path(datadir, format, subdir)
   dir.create(datadir, recursive=TRUE, showWarnings=FALSE)
   for (name in names(rdata)) {
     filename <- file.path(datadir, sprintf("%s.%s", name, format))
+    loginfo("Saving %s", filename, logger="extractoR.data")
     FUNC(rdata[[name]], file=filename, ...)
   }
 }
 
-LoadRData <- function(datadir, format="rds", FUNC=readRDS, ...) {
-  datadir <- file.path(datadir, format)
+LoadRData <- function(datadir, subdir=".", format="rds", FUNC=readRDS, ...) {
+  datadir <- file.path(datadir, format, subdir)
   extension <- sprintf("\\.%s$", format)
   files <- grep(extension, dir(datadir), value=TRUE)
-  rdata <- lapply(files, function(f) FUNC(file=file.path(datadir, f), ...))
+  rdata <- lapply(files, function(f) {
+    loginfo("Loading %s", f, logger="extractoR.data")
+    FUNC(file=file.path(datadir, f), ...)
+  })
   names(rdata) <- sub(extension, "", files)
   rdata
 }
 
-SaveCSV <- function(rdata, datadir) {
+SaveCSV <- function(rdata, datadir, subdir=".") {
   rdata <- rdata[sapply(rdata, inherits, "data.table")]
-  SaveRData(rdata, datadir, format="csv", FUNC=write.csv, row.names=FALSE)
+  SaveRData(rdata, datadir, subdir, format="csv", FUNC=write.csv,
+            row.names=FALSE)
 }
 
-LoadCSV <- function(datadir) {
-  cran <- LoadRData(datadir, format="csv", FUNC=read.csv, stringsAsFactor=FALSE)
+LoadCSV <- function(datadir, subdir=".") {
+  cran <- LoadRData(datadir, subdir, format="csv", FUNC=read.csv,
+                    stringsAsFactor=FALSE)
   lapply(cran, as.data.table)
 }
 
-SaveJSON <- function(rdata, datadir) {
+SaveJSON <- function(rdata, datadir, subdir=".") {
   rdata <- rdata[sapply(rdata, inherits, "list")]
-  SaveRData(rdata, datadir, format="json",
+  SaveRData(rdata, datadir, subdir, format="json",
             FUNC=function(x, file) cat(toJSON(x), file=file))
 }
 
-LoadJSON <- function(datadir) {
-  LoadRData(datadir, format="json", FUNC=fromJSON)
+LoadJSON <- function(datadir, subdir=".") {
+  LoadRData(datadir, subdir, format="json", FUNC=fromJSON)
 }
 
-SaveYAML <- function(rdata, datadir) {
+SaveYAML <- function(rdata, datadir, subdir=".") {
   rdata <- rdata[sapply(rdata, inherits, "list")]
-  SaveRData(rdata, datadir, format="yml",
+  SaveRData(rdata, datadir, subdir, format="yml",
             FUNC=function(x, file) cat(as.yaml(x), file=file))
 }
 
-LoadYAML <- function(datadir) {
-  LoadRData(datadir, format="yml", FUNC=function(file) yaml.load_file(file))
+LoadYAML <- function(datadir, subdir=".") {
+  LoadRData(datadir, subdir, format="yml", FUNC=yaml.load_file)
 }
