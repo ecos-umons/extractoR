724e173d655e71a63e0ddd27f93ac8a14d378004
Maëlick Claes <himself@maelick.net>
2016-10-12 17:57:49 +0200

    Github DESCRIPTION files extraction using ls-tree and cat-file instead of checkout

--- extractoR.description/R/descfiles.R
+++ extractoR.description/R/descfiles.R
@@ -4,43 +4,38 @@ GuessEncoding <- function(filename) {
   strsplit(system(cmd, intern=TRUE), " ")[[1]][2]
 }
 
-ReadDescfile <- function(filename, package=NA, version=NA) {
-  if (file.exists(filename)) {
-    descfile <- read.dcf(filename)
-    values <- as.vector(descfile[1, ])
+ReadDescfile <- function(filename) {
+  descfile <- read.dcf(filename)
+  values <- as.vector(descfile[1, ])
+  encoding <- "utf8"
+  if (inherits(filename, "character")){
     encoding <- GuessEncoding(filename)
-    if ("Encoding" %in% colnames(descfile)) {
-      encoding <- descfile[colnames(descfile) == "Encoding"]
-    }
-    values <- iconv(as.vector(descfile[1, ]), encoding, "utf8")
-    n <- ncol(descfile)
-    res <- as.data.table(list(key=colnames(descfile), value=values))
-    if (is.na(package) | is.na(version)) res
-    else cbind(data.table(package=package, version=version), res)
   }
+  if ("Encoding" %in% colnames(descfile)) {
+    encoding <- descfile[colnames(descfile) == "Encoding"]
+  }
+  if (!tolower(encoding) %in% c("utf8", "utf-8", "cannot")) {
+    values <- iconv(as.vector(descfile[1, ]), encoding, "utf8")
+  }
+  as.data.table(list(key=colnames(descfile), value=values))
 }
 
 ReadCRANDescfile <- function(package, version, datadir) {
   loginfo("Parsing CRAN DESCRIPTION file from %s %s",
           package, version, logger="description.cran")
-  filename <- file.path(datadir, package, version, package, "DESCRIPTION")
-  descfile <- ReadDescfile(filename, package, version)
-  if (!is.null(descfile)) cbind(data.table(source="cran"), descfile)
+  ReadDescfile(file.path(datadir, package, version, package, "DESCRIPTION"))
 }
 
-ReadGithubDescfile <- function(package, ref, datadir) {
-  loginfo("Parsing Github DESCRIPTION file from package %s %s",
-          package, ref, logger="description.github")
-  repo.name <- ParseGithubRepositoryName(package)
+ReadGithubDescfile <- function(repository, ref, datadir) {
+  loginfo("Parsing Github DESCRIPTION file from %s %s",
+          repository, ref, logger="description.github")
+  repo.name <- ParseGithubRepositoryName(repository)
   RunGit(function() {
     filename <- file.path(repo.name$subdir, "DESCRIPTION")
-    status <- system2("git", c("checkout", ref, filename))
-    if (!status) {
-      res <- tryCatch(ReadDescfile(filename), error=function(e) NULL)
-      system2("git", c("checkout", "HEAD", filename))
-      if (!is.null(res)) {
-        cbind(data.table(source="github", package, version=ref), res)
-      }
+    f <- system2("git", c("ls-tree", ref, filename), stdout=TRUE)
+    if (length(f)) {
+      args <- c("cat-file", "-p", strsplit(f, " |\t")[[1]][3])
+      ReadDescfile(textConnection(system2("git", args, stdout=TRUE)))
     }
   }, file.path(datadir, repo.name$owner, repo.name$repository))
 }
@@ -49,15 +44,17 @@ Descfiles <- function(index, datadir) {
   res <- rbindlist(mapply(function(src, repository, version) {
     dir <- file.path(datadir, src)
     if (src == "cran") {
-      ReadCRANDescfile(repository, version, file.path(dir, "packages"))
+      res <- ReadCRANDescfile(repository, version, file.path(dir, "packages"))
     } else if (src == "github") {
-      ReadGithubDescfile(repository, version, file.path(dir, "repos"))
+      res <- ReadGithubDescfile(repository, version, file.path(dir, "repos"))
     } else {
       stop(sprintf("Unknown source: %s", src))
+    }
+    if (!is.null(res) && nrow(res)) {
+      cbind(data.table(source=src, repository, version), res)
     }
   }, index$source, index$repository, index$ref, SIMPLIFY=FALSE))
   re <- "^\\s*(\\d+([-.]\\d+)*\\S*)(\\s.*)?$"
   res[key == "Version", value := sub(re, "\\1", value)]
-  setnames(res, c("package", "version"), c("repository", "ref"))
   res
 }
