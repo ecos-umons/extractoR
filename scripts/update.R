33587d8e2c9b9bf547b8022fd807d26e1ef1f9f5
Maëlick Claes <himself@maelick.net>
2016-11-10 09:23:05 +0100

    update script fix

--- scripts/update.R
+++ scripts/update.R
@@ -8,7 +8,7 @@ mirror <- getCRANmirrors()$URL[grepl("0-Cloud", getCRANmirrors()$Name,
                                      ignore.case=FALSE)][1]
 
 cran.params <- list(cran.mirror=mirror)
-github.params <- list(filter=FALSE, fetch=TRUE, update=FALSE, cluster.size=4)
+github.params <- list(filter=TRUE, fetch=TRUE, update=TRUE, cluster.size=4)
 
 system.time(UpdateIndex(datadir, "rdata", "mongodb://localhost",
                         cran.params, github.params))
